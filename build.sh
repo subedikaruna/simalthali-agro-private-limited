243005486ba6da303f0f0da51b3eca897c299efc
subedikaruna <karyanna7@gmail.com>
2026-08-23 01:47:24 +0545

    Create admin user during deployment

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# Render runs this automatically on every deploy.
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py create_admin
